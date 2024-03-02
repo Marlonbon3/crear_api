8d6c2501d5f321f36e690cb6c884bedd8c05b33e
Marlonbon3 <al22304010@utslrc.edu.mx>
2024-03-01 22:00:35 -0700

    Initial commit

---
+++ src/routes/item.ts
@@ -0,0 +1,13 @@
+import { Router, Response, Request } from "express";
+import { getItems, getItem, postItem, updateItem, deleteItem } from "../controllers/item";
+import { logMiddleware } from "../middleware/log";
+
+const router = Router()
+
+router.get("/",getItems);
+router.get("/:id", logMiddleware,getItem);
+router.post("/:",postItem);
+router.put("/:id",updateItem);
+router.delete("/:id",deleteItem);
+
+export {router}
